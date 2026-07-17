47a47d0759a93302fd0af1047e61e4473f0cfb74
alvin6998 <chongenpeng@gmail.com>
2026-07-17 15:12:00 +0800

    新增 sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://alvinhomelab.idv.tw',
+  integrations: [sitemap()],
 });
